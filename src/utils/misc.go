a606000d5e170c9b0a6f1377dcde03ba36aef8ed
Ben Busby <contact@benbusby.com>
2023-07-06 16:48:58 -0600

    Add ability to generate passphrases for file URLs
    After a user has uploaded a file, they'll be given a random series of
    three words that will be used to access the file elsewhere. These are
    easily readable (English) words that should be simple to memorize if
    needed.

--- src/utils/misc.go
+++ src/utils/misc.go
@@ -1,8 +1,14 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
 
 const letters string = "abcdefghijklmnopqrstuvwxyz"
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func GenRandomString(n int) string {
 	b := make([]byte, n)
@@ -10,4 +16,20 @@ func GenRandomString(n int) string {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(b)
+}
+
+func GenFilePath() string {
+	min := 0
+	max := len(EFFLongWordList)
+
+	var words []string
+
+	i := 0
+	for i < 3 {
+		idx := r.Intn(max-min+1) + min
+		words = append(words, EFFLongWordList[idx])
+		i++
+	}
+
+	return strings.Join(words, ".")
 }
