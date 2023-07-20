25ca3e8125aaf146a5324dd909c5e7269b234ab2
Ben Busby <contact@benbusby.com>
2023-07-20 17:28:23 -0600

    [DB] Create expiry table to track downloads and lifetime
    Each file must have an expiration time and optionally a number of
    downloads before the file is deleted from B2 and yeetfile tables.
    
    The expiration time should be bound between one minute to 30 (?) days in
    the future.

---
+++ db/expiry.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"log"
+	"time"
+)
+
+type FileExpiry struct {
+	ID        string
+	Downloads int
+	Date      time.Time
+}
+
+func SetFileExpiry(id string, downloads int, date time.Time) {
+	s := `INSERT INTO expiry
+	      (id, downloads, date)
+	      VALUES ($1, $2, $3)`
+	_, err := db.Exec(s, id, downloads, date)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func DecrementDownloads(id string) int {
+	s1 := `UPDATE expiry
+	      SET downloads = downloads - 1
+	      WHERE id=$1
+	      AND downloads > 0`
+	_, err := db.Exec(s1, id)
+	if err != nil {
+		panic(err)
+	}
+
+	s2 := `SELECT downloads FROM expiry WHERE id=$1 AND downloads >= 0`
+	rows, err := db.Query(s2, id)
+	if err != nil {
+		log.Fatalf("Error retrieving download counter: %v", err)
+		return -1
+	}
+
+	if rows.Next() {
+		var downloads int
+		err = rows.Scan(&downloads)
+
+		if err != nil {
+			return downloads
+		}
+	}
+
+	return -1
+}
