7566db6341a79eeac8e9475931684784c21e9395
Ben Busby <contact@benbusby.com>
2025-01-15 14:36:35 -0700

    Bump version to 0.0.1

--- shared/constants/constants.go
+++ shared/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 type UpgradeDuration string
 
 const (
-	VERSION = "1.0.1"
+	VERSION = "0.0.1"
 
 	// JSSessionKey determines the hardcoded string that gets replaced in db.ts in
 	// order to have a semi-random value used in encrypting the user's key pair in
